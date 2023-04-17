c83208f2922fe2815097feda1599d1722e26349f
Yordan Nikolov <jnikolow93@gmail.com>
2023-04-17 08:04:13 +0100

    drop-down menu and search style are done

--- src/Menus/MainMenu/MainMenu.js
+++ src/Menus/MainMenu/MainMenu.js
@@ -2,7 +2,6 @@ import './mainMenu.css'
 import React, { useState } from 'react'
 import { Link, } from 'react-router-dom'
 
-import DropDownMenu from '../DropDownMenu/DropDownMenu'
 
 
 import portal from '../../public/siteIcons/portal.png'
@@ -13,8 +12,9 @@ const MainMenu = () => {
     const [showMenu, setShowMenu] = useState(false);
     const [onTarget, setTarget] = useState(false)
 
-    const dropDownMenu = () => {
+    const dropDownMenu = (e) => {
 
+        console.log(e)
         setShowMenu(!showMenu);
     }
 
@@ -24,6 +24,7 @@ const MainMenu = () => {
 
         if (e.target.className == 'main-menu-links') {
             e.target.offsetParent.childNodes.forEach((elementTarget) => {
+                console.log(elementTarget)
                 elementTarget.className = 'main-menu-links'
             })
             e.target.className = 'main-menu-links seleced'
@@ -59,55 +60,60 @@ const MainMenu = () => {
                         <img className='vectorArrowDown' src={vectorArrowDown}></img>
                     </button>
                 </span>
-
-                {showMenu && (
-                    <div className="dropdown-menu">
-                        <div className='main-categories'>
-                            <Link className='drop-down-menu-links' to=''>Discussions</Link>
-                            <Link className='drop-down-menu-links' to=''>Buyer`s guides</Link>
-                        </div>
-                        <div className='cost-of-livings'>
-                            <Link className='drop-down-menu-links' to=''>Cost of livings</Link>
-                        </div>
-                        <div className='second-categories first-collum-second-catergories' >
-                            <Link className='drop-down-menu-links all-catergories' to=''>All Categories</Link>
-                            <Link className='drop-down-menu-links' to=''>Home & Livings</Link>
-                            <Link className='drop-down-menu-links' to=''>Family & Kids</Link>
-                            <Link className='drop-down-menu-links' to=''>Garden & Do it Yourself</Link>
-                            <Link className='drop-down-menu-links' to=''>Groceries</Link>
-                            <Link className='drop-down-menu-links' to=''>Travel</Link>
-                            <Link className='drop-down-menu-links' to=''>Sports & Outdors</Link>
-                            <Link className='drop-down-menu-links' to=''>BroadBand & Phone Contracts</Link>
-                            <Link className='drop-down-menu-links' to=''>Finance & Insurance</Link>
-                        </div>
-                        <div className='second-categories second-collum-second-catergories'>
-                            <Link className='drop-down-menu-links' to=''>Electronics</Link>
-                            <Link className='drop-down-menu-links' to=''>Fashion & Accessories</Link>
-                            <Link className='drop-down-menu-links' to=''>Culture & Leisure</Link>
-                            <Link className='drop-down-menu-links' to=''>Services & Contracts</Link>
-                            <Link className='drop-down-menu-links' to=''>Healt & Beaty</Link>
-                            <Link className='drop-down-menu-links' to=''>Gaming</Link>
-                            <Link className='drop-down-menu-links' to=''>Car 7 Motorcycle</Link>
-                        </div>
-
-                        <div className='all-merchants'>
-                            <Link className='drop-down-menu-links all-catergories' to=''>All merchants</Link>
-
-                            <Link className='drop-down-menu-links' to=''>Ebay</Link>
-                            <Link className='drop-down-menu-links' to=''>Currys</Link>
-                            <Link className='drop-down-menu-links' to=''>Amazon</Link>
-                            <Link className='drop-down-menu-links' to=''>Asda</Link>
-                            <Link className='drop-down-menu-links' to=''>Very</Link>
-                            <Link className='drop-down-menu-links' to=''>Homebase</Link>
-                            <Link className='drop-down-menu-links' to=''>Boots</Link>
-                            <Link className='drop-down-menu-links' to=''>Argos</Link>
-                            <Link className='drop-down-menu-links' to=''>Halfords</Link>
-                        </div>
-
-                    </div>
-                )}
-
             </nav>
+            {showMenu && (
+                <div className="dropdown-menu slide-in-top">
+                    <div className='main-categories'>
+                        <Link className='drop-down-menu-links' to=''>Discussions</Link>
+                        <Link className='drop-down-menu-links' to=''>Buyer`s guides</Link>
+                    </div>
+                    <div className='cost-of-livings'>
+                        <Link className='drop-down-menu-links' to=''>Cost of livings</Link>
+                    </div>
+                    <div className='second-categories first-collum-second-catergories' >
+                        <Link className='drop-down-menu-links all-catergories' to=''>All Categories</Link>
+                        <Link className='drop-down-menu-links' to=''>Home & Livings</Link>
+                        <Link className='drop-down-menu-links' to=''>Family & Kids</Link>
+                        <Link className='drop-down-menu-links' to=''>Garden & Do it Yourself</Link>
+                        <Link className='drop-down-menu-links' to=''>Groceries</Link>
+                        <Link className='drop-down-menu-links' to=''>Travel</Link>
+                        <Link className='drop-down-menu-links' to=''>Sports & Outdors</Link>
+                        <Link className='drop-down-menu-links' to=''>BroadBand & Phone Contracts</Link>
+                        <Link className='drop-down-menu-links' to=''>Finance & Insurance</Link>
+                    </div>
+                    <div className='second-categories second-collum-second-catergories'>
+                        <Link className='drop-down-menu-links' to=''>Electronics</Link>
+                        <Link className='drop-down-menu-links' to=''>Fashion & Accessories</Link>
+                        <Link className='drop-down-menu-links' to=''>Culture & Leisure</Link>
+                        <Link className='drop-down-menu-links' to=''>Services & Contracts</Link>
+                        <Link className='drop-down-menu-links' to=''>Healt & Beaty</Link>
+                        <Link className='drop-down-menu-links' to=''>Gaming</Link>
+                        <Link className='drop-down-menu-links' to=''>Car 7 Motorcycle</Link>
+                    </div>
+
+                    <div className='all-merchants'>
+                        <Link className='drop-down-menu-links all-catergories' to=''>All merchants</Link>
+
+                        <Link className='drop-down-menu-links mechants-only' to=''>Ebay</Link>
+                        <Link className='drop-down-menu-links mechants-only' to=''>Currys</Link>
+                        <Link className='drop-down-menu-links mechants-only' to=''>Amazon</Link>
+                        <Link className='drop-down-menu-links mechants-only' to=''>Mark & Spencer</Link>
+                        <Link className='drop-down-menu-links mechants-only' to=''>Asda</Link>
+                    </div>
+                    <div className='second-part-merchants'>
+                        <Link className='drop-down-menu-links mechants-only' to=''>Very</Link>
+                        <Link className='drop-down-menu-links mechants-only' to=''>Homebase</Link>
+                        <Link className='drop-down-menu-links mechants-only' to=''>Boots</Link>
+                        <Link className='drop-down-menu-links mechants-only' to=''>Argos</Link>
+                        <Link className='drop-down-menu-links mechants-only' to=''>Halfords</Link>
+                    </div>
+
+                </div>
+            )}
+
+
+
+
             <div className='search-bar-container'>
                 <input className='search-bar' ></input>
             </div>
