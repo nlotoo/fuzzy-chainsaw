95b31a9f15478a656a37d855d20053ecabdbd9b4
Yordan Nikolov <jnikolow93@gmail.com>
2023-05-25 21:07:34 +0100

    change arch and training graphQL mutation

--- server/app.js
+++ server/app.js
@@ -12,235 +12,26 @@ expressConfig(app)
 app.use(router)
 
 const { ApolloServer, gql } = require('apollo-server');
+const _ = require('lodash');
+const { typeDefs } = require("./schema/type-defs");
+const { resolvers } = require('./schema/resover.js')
 // import { ApolloServer } from '@apollo/server';
 // const { gql } = require("apollo-server-ex")
 // const { gql } = require('apollo-server-express');
 
-const typeDefs = gql`
 
-type Magazine{
 
-    id:ID
 
-    title: String
 
-    author: String
 
-    year: Int
 
-    country: Country
-}
 
-type Book {
 
 
-  title: String
 
-  author: String
 
-}
 
-type Customer{
 
-    name:String
-
-    year:String
-
-    gender: Gender
-
-    collection:[Magazine]
-}
-
-
-type Query {
-
-  books: [Book]
-
-  magazines: [Magazine]
-
-  magaz(id:ID):Magazine
-
-  buyers:[Customer]
-
-  csearch(name:String):Customer
-
-}
-
-enum Gender{
-
-    MALE
-    FEMALE
-}
-
-
-enum  Country{
- SPAIN
- GERMAN
- ITALY
- BRAZIL
-}
-
-`;
-
-
-const customers = [
-
-    {
-        name: 'Pesho',
-        year: '1990',
-        gender: 'MALE',
-        collection: [{
-            id: '4',
-
-            title: 'Amazon',
-
-            author: 'Bai Ivan 2',
-
-            year: 2022,
-
-            country: 'BRAZIL'
-
-
-        },
-        {
-            id: '2',
-
-            title: 'Forbse',
-
-            author: 'Danw Jhone',
-
-            year: 2023,
-
-            country: 'GERMAN'
-
-        },
-
-
-        ]
-    }
-]
-
-const magazines = [
-    {
-        id: '2',
-
-        title: 'Forbse',
-
-        author: 'Danw Jhone',
-
-        year: 2023,
-
-        country: 'GERMAN',
-        collection: null
-
-
-    },
-
-    {
-        id: '3',
-
-        title: 'Hai Club',
-
-        author: 'Bai Ivan',
-
-        year: 2022,
-
-        country: 'SPAIN',
-        collection: null
-
-
-
-    },
-
-
-    {
-        id: '4',
-
-        title: 'Amazon',
-
-        author: 'Bai Ivan 2',
-
-        year: 2022,
-
-        country: 'BRAZIL',
-        collection: null
-
-
-    }
-
-]
-
-const books = [
-
-    {
-
-        title: 'The Awakening',
-
-        author: 'Kate Chopin',
-
-    },
-
-    {
-
-        title: 'City of Glass',
-
-        author: 'Paul Auster',
-
-    },
-
-];
-
-const _ = require('lodash');
-
-
-const resolvers = {
-
-    Query: {
-
-        books: () => books,
-
-        magazines: () => magazines,
-
-        magaz: (parent, args) => {
-
-            const id = args.id;
-            const magazine = magazines.find((m) => {
-                console.log(id)
-
-                if (m.id === args.id) {
-                    return m
-                }
-            });
-            // console.log(magazine)
-            return magazine
-
-        },
-
-
-        buyers: () => customers,
-
-        csearch: (parent, args) => {
-
-
-            // console.log(args)
-
-            const name = args.name
-            // const result = 
-            return customers.find((data) => {
-
-                return data.collection.filter((m) => m.country !== 'BRAZIL')
-
-
-            })
-
-            // return result
-
-        }
-
-    },
-
-
-};
 
 const server = new ApolloServer({ typeDefs, resolvers })
 server.listen().then(({ url }) => {
---
+++ server/schema/type-defs.js
@@ -0,0 +1,97 @@
+const { gql } = require('apollo-server');
+
+
+const typeDefs = gql`
+
+type Magazine{
+
+    id:ID
+
+    title: String
+
+    author: String
+
+    year: Int
+
+    country: Country
+}
+
+type Book {
+
+
+  title: String
+
+  author: String
+
+}
+
+type Customer{
+
+    name:String
+
+    year:String
+
+    gender: Gender
+
+    collection:[Magazine]
+}
+
+
+type Query {
+
+  books: [Book]
+
+  magazines: [Magazine]
+
+  magaz(id:ID):Magazine
+
+  buyers:[Customer]
+
+  csearch(name:String):Customer
+
+
+ 
+},
+
+input CreateBookInput {
+
+  title: String
+
+  author: String
+}
+
+input updateBookName{
+  title:String
+  newUserTitle:String 
+}
+
+
+type Mutation {
+
+  createBook( input:CreateBookInput):Book
+  updateBookName(input:updateBookName):Book
+  deleteBook(title: String):Book
+
+}
+
+
+
+
+enum Gender{
+
+    MALE
+    FEMALE
+}
+
+
+enum  Country{
+ SPAIN
+ GERMAN
+ ITALY
+ BRAZIL
+}
+
+`;
+
+
+module.exports = { typeDefs }
