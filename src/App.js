47e254b6b9046fc7d692901208a12e512dcc28a4
Yordan Nikolov <jnikolow93@gmail.com>
2023-05-26 08:00:53 +0100

    training query on front-end

--- src/App.js
+++ src/App.js
@@ -1,38 +1,48 @@
 
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
+
 import MainMenu from './Menus/MainMenu/MainMenu.js'
 import SecondMenu from './Menus/SecondMenu/SecondMenu';
 import Submit from './Components/Submit/Submit'
+import GraphQL from './Components/GraphQl/GraphQL';
 
 
 import { useStateContext } from './context/StateContext';
 import useScreenSize from './Hooks/useScreenSize';
-import GraphQL from './Components/GraphQl/GraphQL';
 
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client'
 
 
 
 function App() {
   let { AuthMenuState } = useStateContext();
+  let screenSize = useScreenSize();
 
-  let screenSize = useScreenSize()
+  const client = new ApolloClient({
+    cache: new InMemoryCache(),
+    uri: 'http://localhost:4000/graphql',
+  });
+
 
   return (
+    <ApolloProvider client={client}>
 
-    <div className={AuthMenuState ? 'trasnp-bg' : undefined}
-    >
-      <BrowserRouter>
-        <MainMenu></MainMenu>
-        <SecondMenu></SecondMenu>
-        <div>asdds</div>
-        <Routes>
-          <Route path="/" element={<div> width: {screenSize.width}- - height: {screenSize.height}</div>} />
-          <Route path='/submit' element={<Submit />}></Route>
-          <Route path='/test' element={<GraphQL />}></Route>
-        </Routes>
-      </BrowserRouter>
-    </div >
+      <div className={AuthMenuState ? 'trasnp-bg' : undefined}
+      >
+        <BrowserRouter>
+          <MainMenu></MainMenu>
+          <SecondMenu></SecondMenu>
+          <div>asdds</div>
+          <Routes>
+            <Route path="/" element={<div> width: {screenSize.width}- - height: {screenSize.height}</div>} />
+            <Route path='/submit' element={<Submit />}></Route>
+            <Route path='/test' element={<GraphQL />}></Route>
+          </Routes>
+        </BrowserRouter>
+      </div >
+    </ApolloProvider>
+
   );
 }
 
--- src/Components/GraphQl/GraphQL.js
+++ src/Components/GraphQl/GraphQL.js
@@ -1,36 +1,148 @@
-import React from 'react'
-
+import React, { useState } from 'react'
 import { gql } from '@apollo/client';
-import { useQuery } from '@apollo/client';
+import { useQuery, useLazyQuery } from '@apollo/client';
 
 const GraphQL = () => {
 
-    const GET_USERS_QUERY = gql`
+  const GET_BOOKS_QUERY = gql`
   query {
-    users {
-      id
-      name
+    books {
+      author
+      title
     }
   }
 `;
 
-    const { loading, error, data } = useQuery(GET_USERS_QUERY);
-    console.log(data)
+
+  const GET_MAGAZINES_QUERY = gql`
+  query  {
+  magazines {
+    author
+    country
+    id
+    title
+    year
+  }
+}
+`;
+
+
+  const GET_BUYERS_QUERY = gql`
+query Buyers {
+  buyers {
+    year
+    name
+    gender
+    collection {
+      author
+      country
+      id
+      title
+      year
+    }
+  }
+}`;
+
+
+  const GET_MAGAZINE_BY_NAME = gql`
+
+  query Magaz($title:String){
+    query magaz(title:$title){
+    title
+    }
+  }
+`;
+
+  const { loading, error, data } = useQuery(GET_BOOKS_QUERY);
+  const { loading: magazinesLoading, error: magazinesError, data: magazinesData } = useQuery(GET_MAGAZINES_QUERY);
+  const { loading: buyersLoading, error: buyersError, data: buyersData } = useQuery(GET_BUYERS_QUERY);
+
+
+  let { magazineSearched, setMagazinSearched } = useState('')
+  const [concreteMagazine, { data: concreteMagazineData, error: concreteMagazineError }] = useLazyQuery(GET_MAGAZINE_BY_NAME)
+
+  // console.log(buyersData)
+
+
+  const OnchangesettingData = (e) => {
+    console.log(e.target.value)
+    // console.log(e.traget.value)
+    setMagazinSearched(e.target.value)
+  }
 
 
 
-    return (
-        <div>GraphQL
-            <select name="something" id="test">
-                <option value="volvo">Volvo</option>
-            </select>
+  return (
+    <div style={{ display: 'flex' }}>
 
-            <div>
-                <p>NEW REQUEST</p>
+      <div >
+        <h3>Books</h3>
+        {loading && <div>LOADING...</div>}
 
-            </div>
+        {data?.books && data?.books.map((book, index) => {
+          return <ul key={index}>
+            <li>title: {book.title}</li>
+            <li>author: {book.author}</li>
+          </ul>
+        })}
+      </div>
+
+      <div>
+        <h3>Magazines</h3>
+        {magazinesData?.magazines && magazinesData?.magazines.map((magazines, index) => {
+          // console.log(magazines)
+          return <ul key={index}>
+            <li>id: {magazines.id}</li>
+            <li>title: {magazines.title}</li>
+            <li>author: {magazines.author}</li>
+            <li>age: {magazines.year}</li>
+            <li>contrty: {magazines.country}</li>
+          </ul>
+        })}
+      </div>
+      <div>
+        <h3>Buyers</h3>
+        <div>
+          {
+            buyersData?.buyers &&
+            buyersData?.buyers.map((buyer, index) => {
+              return <ul key={index}>
+                <li>year: {buyer.year}</li>
+                <li>name {buyer.name}</li>
+                <li>gender: {buyer.gender}</li>
+                <li>buyer collection:
+                  {buyer?.collection.map((x) => {
+                    { console.log(x) }
+                    return <ul>
+                      <li>id: {x.id}</li>
+                      <li>title: {x.title}</li>
+                      <li>author: {x.author}</li>
+                      <li>age: {x.year}</li>
+                      <li>contrty: {x.country}</li>
+                    </ul>
+
+                  })
+                  }
+                </li>
+
+              </ul>
+            })
+
+          }
         </div>
-    )
+      </div>
+
+      <div>
+        {magazineSearched}
+        <input type='text' placeholder='add..' onChange={OnchangesettingData} />
+        <button onClick={
+          () => concreteMagazine({ variables: { title: magazineSearched } })
+        }>Fetch data</button>
+      </div>
+
+
+    </div >
+  )
 }
 
 export default GraphQL
