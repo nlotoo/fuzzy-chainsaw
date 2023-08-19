876f502393f3df439cc8f4be72d978418f6f9947
Yordan Nikolov <jnikolow93@gmail.com>
2023-08-20 00:23:05 +0100

    update deal  detail page

--- src/Components/GetDeal/GetDealPage.js
+++ src/Components/GetDeal/GetDealPage.js
@@ -1,17 +1,18 @@
 import React, { useEffect, useState } from 'react'
 
-import AsidePost from '../AsidePost/AsidePost';
 import './getDealpage.css'
 import { gql, useLazyQuery } from '@apollo/client';
-import { useStateContext } from '../../context/StateContext';
+
+// import '../../public/uploads'
 
 import Spinner from 'react-bootstrap/Spinner';
+import { Link } from 'react-router-dom';
 
 
 const GetDealPage = () => {
 
 
-    const GET_POST = gql`
+  const GET_POST = gql`
 query GetPost($getPostId: ID) {
   getPost(id: $getPostId) {
     id
@@ -54,62 +55,82 @@ query GetPost($getPostId: ID) {
 }
 `
 
-    // const { loading, error, data } = useLazyQuery(GET_POST);
-    const [getPostbyID, { loading, data, error }] = useLazyQuery(GET_POST);
 
-    console.log(data)
-    // console.log(error)
+  const [getPostbyID, { loading, data, error }] = useLazyQuery(GET_POST);
 
-    const targetDate = new Date(data?.getPost.createAt);
-   
-    const [timePassed, setTimePassed] = useState({ days: 0, hours: 0, minutes: 0 });
+  // console.log(window.location.pathname.split('/')[2])
+  console.log(data?.getPost)
 
-    const timeCalculate = (targetDate) => {
-        const currentTime = new Date();
-        const timeDifference = currentTime - targetDate;
 
-        const days = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
-        const hours = Math.floor((timeDifference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-        const minutes = Math.floor((timeDifference % (1000 * 60 * 60)) / (1000 * 60));
 
-        setTimePassed({ days, hours, minutes });
+  const targetDate = new Date(data?.getPost.createAt);
+
+  const [timePassed, setTimePassed] = useState({ days: 0, hours: 0, minutes: 0 });
+
+  const timeCalculate = (targetDate) => {
+    console.log(targetDate)
+    const currentTime = new Date();
+    const timeDifference = currentTime - targetDate;
+
+    const days = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((timeDifference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((timeDifference % (1000 * 60 * 60)) / (1000 * 60));
+
+    setTimePassed({ days, hours, minutes });
+  }
+
+
+
+  useEffect(() => {
+    getPostbyID({ variables: { getPostId: window.location.pathname.split('/')[2] } })
+
+    if (data) {
+      timeCalculate(targetDate);
+
     }
-    
-    
-    
-    useEffect(() => {
-        getPostbyID({ variables: { getPostId: '64dd43fa7113c9c7b4d22350' } })
-      
-        if(data){
-            timeCalculate(targetDate);
-
-        }
 
 
+  }, [data]);
 
-        
-    }, [targetDate]);
+  return (
+    <div className='get-deal-page-container'>
+      {loading == true
+        ? <Spinner animation="border" className='spinner' />
+        : <div className='get-deal-container'>
+          <div className='get-deal-image-container'>
+            {data !== undefined ?
+              <img src={require(`../../public/uploads/${data?.getPost.images[0].path.split('/')[6]}`)} alt='get-details' className='image-get-details' />
+              :
+              <Spinner animation="grow" />
 
-    return (
-        <div className='get-deal-page-container'>
-            {loading == true
-                ? <Spinner animation="border" className='spinner' />
-                : <div className='get-deal-container'>
-                    <div className='get-deal-image-container'>
-                        here image
-                    </div>
-                    <div>
+            }
+          </div>
 
-                        <div className='post-details-row-one grey' >
-                            <span><b ><i className="fa-regular fa-clock"></i> <i>Posted {timePassed.days > 0 ? `${timePassed.days} days,` : ''}  {timePassed.hours > 0 ? `${timePassed.hours}h,` : ''} {timePassed.minutes}m ago </i>  </b> &nbsp; &nbsp;</span>
 
-                        </div>
-                    </div>
 
-                </div>}
+          <div className='get-deal-detail'>
 
-        </div>
-    )
+            <h1>{data?.getPost.offertDetails.title}</h1>
+
+            <h2 className='price-heading-get-deails' >£<s>{data?.getPost.offertDetails.normalPrice}</s> | £ {data?.getPost.offertDetails.curentPrice}</h2>
+
+
+            <button onClick={() => window.location.replace(`${data?.getPost.link}`)} className='get-deal-btn ' >Get deal</button>
+
+
+            <div className='post-details-row-one grey get-details-time' >
+            <div> <b><i className='grey'>Created by: </i></b>
+              {data?.getPost.owner}
+            </div>
+              <span><b ><i className="fa-regular fa-clock"></i> <i>Posted <br></br> {timePassed.days > 0 ? `${timePassed.days} days,` : ''}  {timePassed.hours > 0 ? `${timePassed.hours}h,` : ''} {timePassed.minutes}m ago </i>  </b> &nbsp; &nbsp;</span>
+
+            </div>
+          </div>
+
+        </div>}
+
+    </div>
+  )
 }
 
 export default GetDealPage
