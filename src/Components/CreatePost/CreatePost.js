12dff85b84345f728c35baa5e69c5787e58694da
Yordan Nikolov <jnikolow93@gmail.com>
2023-09-04 22:55:35 +0100

    Update /create2

--- src/Components/CreatePost/CreatePost.js
+++ src/Components/CreatePost/CreatePost.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 
 import Button from 'react-bootstrap/Button';
@@ -28,6 +28,29 @@ function CreatePost() {
 
     const { Formik } = formik;
 
+    const [categories, setCategorries] = useState([]);
+
+
+    const categoryGetter = (event) => {
+
+
+        if (categories.find((element) => element == event.target.value)) {
+            console.log('ma go')
+            setCategorries(categories.filter(a => a !== event.target.value))
+        } else {
+
+            setCategorries(categories.push(event.target.value))
+
+        }
+
+        console.log(event.target.value)
+        console.log(categories)
+    }
+
+
+    useEffect(() => {
+        console.log(categories)
+    }, [categories])
 
 
     const schema = yup.object().shape({
@@ -35,7 +58,10 @@ function CreatePost() {
         link: yup.string().required('Link is required'),
         desc: yup.string().required('Description is required'),
         curentPrice: yup.string().required('Curent price is required'),
-        // productImage: yup.mixed().required('Image is required'),
+        newPrice: yup.string().required('New price is required'),
+        file: yup.mixed().required('Image is required'),
+        startDate: yup.mixed().required('Start date is required'),
+        endDate: yup.mixed().required('End date is required'),
 
     });
 
@@ -48,10 +74,15 @@ function CreatePost() {
                 desc: '',
                 file: null,
                 curentPrice: '',
-                newPrice:'',
-                startDate:'',
-                endDate:'',
+                newPrice: '',
+                startDate: '',
+                endDate: '',
 
+
+                homeAndLivings: '',
+                familyAndKids: '',
+                gardeDoItYourself: '',
+                groceries: '',
             }}
 
             // onSubmit={console.log}
@@ -60,7 +91,7 @@ function CreatePost() {
 
 
 
-                // setSubmitting(false);
+                setSubmitting(false);
                 console.log(values)
                 // console.log(Formik)
             }}
@@ -73,194 +104,240 @@ function CreatePost() {
 
 
                 < div className='create-post-container'>
-            <Form clasName='create-post-form-container2' noValidate onSubmit={handleSubmit}>
-                <h3 id="detailsOfUser">Details of product</h3>
-                {!switcherButton && <Col className="mb-3">
-                    <Form.Group md="4" controlId="validationFormik01">
-                        <Form.Label>Title</Form.Label>
-                        <Form.Control
-                            type="text"
-                            name="title"
-                            value={values.title}
-                            onChange={handleChange}
-                            isValid={touched.title && !errors.title}
+                    <Form clasName='create-post-form-container2' noValidate onSubmit={handleSubmit}>
+                        <h3 id="detailsOfUser">Details of product</h3>
+                        {!switcherButton && <Col className="mb-3">
+                            <Form.Group md="4" controlId="validationFormik01">
+                                <Form.Label>Title</Form.Label>
+                                <Form.Control
+                                    type="text"
+                                    name="title"
+                                    value={values.title}
+                                    onChange={handleChange}
+                                    isValid={touched.title && !errors.title}
 
-                            isInvalid={!!errors.title}
-                        />
+                                    isInvalid={!!errors.title}
+                                />
 
-                        <Form.Control.Feedback type="invalid">
-                            {errors.title}
-                        </Form.Control.Feedback>
-                        <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
-                    </Form.Group>
+                                <Form.Control.Feedback type="invalid">
+                                    {errors.title}
+                                </Form.Control.Feedback>
 
-                    <Form.Group md="4" controlId="validationFormik02">
-                        <Form.Label>Link</Form.Label>
-                        <Form.Control
-                            type="text"
-                            name="link"
+                            </Form.Group>
+
+                            <Form.Group md="4" controlId="validationFormik02">
+                                <Form.Label>Link</Form.Label>
+                                <Form.Control
+                                    type="text"
+                                    name="link"
 
 
-                            value={values.link}
-                            onChange={handleChange}
-                            isValid={touched.link && !errors.link}
-                            isInvalid={!!errors.link}
-                        />
+                                    value={values.link}
+                                    onChange={handleChange}
+                                    isValid={touched.link && !errors.link}
+                                    isInvalid={!!errors.link}
+                                />
 
-                        <Form.Control.Feedback type="invalid">
-                            {errors.link}
-                        </Form.Control.Feedback>
-                        <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
-                    </Form.Group>
+                                <Form.Control.Feedback type="invalid">
+                                    {errors.link}
+                                </Form.Control.Feedback>
 
-                    <Form.Group md="4" controlId="validationFormik03">
-                        <Form.Label>Description</Form.Label>
 
-                        <Form.Control
-                            className='text-area-ofert-details'
-                            as="textarea"
-                            type="text"
-                            placeholder="Describe the product here..."
-                            aria-describedby="inputGroupPrepend"
-                            name="desc"
-                            value={values.desc}
-                            onChange={handleChange}
+                            </Form.Group>
 
-                            isInvalid={!!errors.desc}
-                        />
-                        <Form.Control.Feedback type="invalid">
-                            {errors.desc}
-                        </Form.Control.Feedback>
-                        <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+                            <Form.Group md="4" controlId="validationFormik03">
+                                <Form.Label>Description</Form.Label>
 
-                    </Form.Group>
+                                <Form.Control
+                                    className='text-area-ofert-details'
+                                    as="textarea"
+                                    type="text"
+                                    placeholder="Describe the product here..."
+                                    aria-describedby="inputGroupPrepend"
+                                    name="desc"
+                                    value={values.desc}
+                                    onChange={handleChange}
+                                    isValid={touched.desc && !errors.desc}
+                                    isInvalid={!!errors.desc}
+                                />
+                                <Form.Control.Feedback type="invalid">
+                                    {errors.desc}
+                                </Form.Control.Feedback>
 
 
 
-                    <Form.Group md="4" controlId='validationFormik04'>
-
-                        <Form.Label>Insert Image</Form.Label>
-
-                        <Form.Control
-                            type="file"
-                            as="input"
-                            name='file'
-                            onChange={(event) => {
-                                setFieldValue('file', event.currentTarget.files[0]);
-                            }}
-
-                            isInvalid={!!errors.file}
-
-                        />
+                            </Form.Group>
 
 
 
+                            <Form.Group md="4" controlId='validationFormik04'>
 
+                                <Form.Label>Insert Image</Form.Label>
 
-                        <Form.Control.Feedback type="invalid">
-                            {errors.file}
-                        </Form.Control.Feedback>
-                        <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+                                <Form.Control
+                                    type="file"
+                                    as="input"
+                                    name='file'
+                                    onChange={(event) => {
+                                        setFieldValue('file', event.currentTarget.files[0]);
+                                    }}
+                                    isValid={touched.file && !errors.file}
+                                    isInvalid={!!errors.file}
 
-
-                    </Form.Group>
-
-                    <Button onClick={switcher} type="button">Next step</Button>
-
-                </Col>}
-
-                {switcherButton && <Col className='second-col'>
-
-                    <Form.Group md="4" controlId="validationFormik05">
-                        <Form.Label>Old Price</Form.Label>
-                        <Form.Control
-                            type="number"
-                            name="curentPrice"
-                            value={values.curentPrice}
-                            onChange={handleChange}
-                            isValid={touched.curentPrice && !errors.curentPrice}
-
-                            isInvalid={!!errors.curentPrice}
-                        />
-
-                        <Form.Control.Feedback type="invalid">
-                            {errors.curentPrice}
-                        </Form.Control.Feedback>
-                       
-                    </Form.Group>
-
-                    <Form.Group md="4" controlId="validationFormik06">
-                        <Form.Label>New Price</Form.Label>
-                        <Form.Control
-                            type="number"
-                            name="newPrice"
-                            value={values.newPrice}
-                            onChange={handleChange}
-                            isValid={touched.newPrice && !errors.newPrice}
-
-                            isInvalid={!!errors.newPrice}
-                        />
-
-                        <Form.Control.Feedback type="invalid">
-                            {errors.newPrice}
-                        </Form.Control.Feedback>
-                        
-                    </Form.Group>
+                                />
 
 
 
-                    <Form.Group md="4" controlId="validationFormik07">
-                        <Form.Label>Start Date</Form.Label>
-                        <Form.Control
-                            type="date"
-                            name="startDate"
-                            value={values.startDate}
-                            onChange={handleChange}
-                            isValid={touched.startDate && !errors.startDate}
-
-                            isInvalid={!!errors.startDate}
-                        />
-
-                        <Form.Control.Feedback type="invalid">
-                            {errors.startDate}
-                        </Form.Control.Feedback>
-                        
-                    </Form.Group>
 
 
-                    <Form.Group md="4" controlId="validationFormik08">
-                        <Form.Label>Start Date</Form.Label>
-                        <Form.Control
-                            type="date"
-                            name="endDate"
-                            value={values.endDate}
-                            onChange={handleChange}
-                            isValid={touched.endDate && !errors.endDate}
-
-                            isInvalid={!!errors.endDate}
-                        />
-
-                        <Form.Control.Feedback type="invalid">
-                            {errors.endDate}
-                        </Form.Control.Feedback>
-                        
-                    </Form.Group>
+                                <Form.Control.Feedback type="invalid">
+                                    {errors.file}
+                                </Form.Control.Feedback>
 
 
 
-                    <Button onClick={switcher} type="button">Previus step</Button>
+                            </Form.Group>
 
-                </Col>}
+                            <Button onClick={switcher} type="button">Next step</Button>
+
+                        </Col>}
+
+                        {switcherButton && <Col className='second-col'>
+
+                            <Form.Group md="4" controlId="validationFormik05">
+                                <Form.Label>Old Price</Form.Label>
+                                <Form.Control
+                                    type="number"
+                                    name="curentPrice"
+                                    value={values.curentPrice}
+                                    onChange={handleChange}
+                                    isValid={touched.curentPrice && !errors.curentPrice}
+
+                                    isInvalid={!!errors.curentPrice}
+                                />
+
+                                <Form.Control.Feedback type="invalid">
+                                    {errors.curentPrice}
+                                </Form.Control.Feedback>
+
+                            </Form.Group>
+
+                            <Form.Group md="4" controlId="validationFormik06">
+                                <Form.Label>New Price</Form.Label>
+                                <Form.Control
+                                    type="number"
+                                    name="newPrice"
+                                    value={values.newPrice}
+                                    onChange={handleChange}
+                                    isValid={touched.newPrice && !errors.newPrice}
+
+                                    isInvalid={!!errors.newPrice}
+                                />
+
+                                <Form.Control.Feedback type="invalid">
+                                    {errors.newPrice}
+                                </Form.Control.Feedback>
+
+                            </Form.Group>
 
 
-                <Button type="submit">Submit</Button>
-                
+
+                            <Form.Group md="4" controlId="validationFormik07">
+                                <Form.Label>Start Date</Form.Label>
+                                <Form.Control
+                                    type="date"
+                                    name="startDate"
+                                    value={values.startDate}
+                                    onChange={handleChange}
+                                    isValid={touched.startDate && !errors.startDate}
+
+                                    isInvalid={!!errors.startDate}
+                                />
+
+                                <Form.Control.Feedback type="invalid">
+                                    {errors.startDate}
+                                </Form.Control.Feedback>
+
+                            </Form.Group>
 
 
-            </Form>
-        </div>
-    )
-}
+                            <Form.Group md="4" controlId="validationFormik08">
+                                <Form.Label>Start Date</Form.Label>
+                                <Form.Control
+                                    type="date"
+                                    name="endDate"
+                                    value={values.endDate}
+                                    onChange={handleChange}
+                                    isValid={touched.endDate && !errors.endDate}
+
+                                    isInvalid={!!errors.endDate}
+                                />
+
+                                <Form.Control.Feedback type="invalid">
+                                    {errors.endDate}
+                                </Form.Control.Feedback>
+
+                            </Form.Group>
+
+
+
+                            <Form.Group>
+
+
+                                <Form.Label>Categories</Form.Label>
+                                <Form.Check.Input
+                                    label="Home & Livings"
+                                    name="homeAndLivings"
+                                    value="homeAndLivings"
+                                    type="checkbox"
+                                    onClick={categoryGetter}
+
+                                >
+                                </Form.Check.Input>
+
+
+                                <Form.Check.Input
+                                    label="Family & Kids"
+                                    name="familyAndKids"
+                                    value="familyAndKids"
+                                    type="checkbox"
+                                    onClick={categoryGetter}
+                                >
+                                </Form.Check.Input>
+
+                                <Form.Check.Input
+                                    label="Garden & Do it Yourself"
+                                    name="gardeDoItYourself"
+                                    value="gardeDoItYourself"
+                                    type="checkbox"
+                                >
+                                </Form.Check.Input>
+
+
+                                <Form.Check.Input
+                                    label="Groceries"
+                                    name="groceries"
+                                    value="groceries"
+                                    type="checkbox"
+                                >
+                                </Form.Check.Input>
+
+                            </Form.Group>
+
+
+                            <Button onClick={switcher} id='previous-btn' type="button">Previous  step</Button>
+
+                        </Col>}
+
+
+                        <Button type="submit">Submit</Button>
+
+
+
+                    </Form>
+                </div>
+            )
+            }
         </Formik >
 
     );
